adc2130df2407ad48b42c05b921619835344445a
AdMelioraDev <sj611.dev@gmail.com>
2025-02-11 00:44:45 +0900

    fix: Remove basePath from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   images: {
     unoptimized: true, // GitHub Pages에서는 이미지 최적화 서버를 실행할 수 없으므로
   },
-  basePath: '/admelioradev.github.io', // GitHub Pages의 저장소 이름과 일치해야 함
+  // basePath는 일단 제거
 }
 
 const withMDX = createMDX({
