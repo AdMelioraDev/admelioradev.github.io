0b24585ba8a62515a51be124b4fd40b8a43185cf
AdMelioraDev <sj611.dev@gmail.com>
2025-02-11 01:10:54 +0900

    chore: Update build and deploy configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  output: 'export',  // 정적 HTML 내보내기 설정
+  output: 'export',
+  distDir: 'dist',
   images: {
-    unoptimized: true, // GitHub Pages에서는 이미지 최적화 서버를 실행할 수 없으므로
+    unoptimized: true,
   },
   typescript: {
-    // 타입 체크를 빌드 시에 무시하도록 설정
     ignoreBuildErrors: true,
   },
 }
